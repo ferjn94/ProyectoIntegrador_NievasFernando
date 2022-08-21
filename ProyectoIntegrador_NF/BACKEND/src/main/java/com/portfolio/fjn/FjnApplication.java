54c5b476dc2e26729379f9efe2236c36d9f37151
ferjn94 <100531268+ferjn94@users.noreply.github.com>
2022-08-21 18:07:39 -0300

    creacion inicio backend

---
+++ ProyectoIntegrador_NF/BACKEND/src/main/java/com/portfolio/fjn/FjnApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.fjn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FjnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FjnApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_NF/BACKEND/src/test/java/com/portfolio/fjn/FjnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.fjn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FjnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
