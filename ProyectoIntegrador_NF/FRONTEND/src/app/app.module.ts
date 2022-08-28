fb488c41c374c9f6509878be41257445cc172a74
ferjn94 <100531268+ferjn94@users.noreply.github.com>
2022-08-28 19:04:21 -0300

    Funcion completa de inicio

--- ProyectoIntegrador_NF/FRONTEND/src/app/app.module.ts
+++ ProyectoIntegrador_NF/FRONTEND/src/app/app.module.ts
@@ -13,9 +13,10 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HttpClient, HttpClientModule} from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -35,11 +36,13 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     NgCircleProgressModule.forRoot({}),
     HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
