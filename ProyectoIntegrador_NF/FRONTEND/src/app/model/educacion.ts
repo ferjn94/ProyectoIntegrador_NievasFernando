11190f132c5b5a9873ca327f3a5e8a770310b37e
ferjn94 <100531268+ferjn94@users.noreply.github.com>
2022-10-25 13:25:02 -0300

    elemento educacion

---
+++ ProyectoIntegrador_NF/FRONTEND/src/app/model/educacion.ts
@@ -0,0 +1,10 @@
+export class Educacion {
+    id?: number;
+    nombreE: string;
+    descripcionE: String;
+
+    constructor(nombreE: string, descripcionE:string){
+        this.nombreE = nombreE;
+        this.descripcionE=descripcionE;
+    }
+}
